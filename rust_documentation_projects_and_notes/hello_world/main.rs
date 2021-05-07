0268fbe1c655fa0a7a10e61f6acd706622590be0
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2021-05-07 12:17:50 -0700

    hello world

---
+++ rust_documentation_projects_and_notes/hello_world/main.rs
@@ -0,0 +1,6 @@
+//in rust, fn denotes a function
+// more importantly, the main() function is always the first function to run in any project
+fn main() {
+    println!("hello world!"); //to call a function with print, use println() without the !
+    //the ! tells rust it's printing a macro, which will be discussed later
+}
